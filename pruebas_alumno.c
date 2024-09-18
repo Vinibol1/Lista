acc8ddf0d52561e91248e23cddaf0de6230b4e71
Vinibol1 <tportella@fi.uba.ar>
2024-09-17 23:13:40 -0300

    Añadir .clang y lista_cantidad_elementos

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,47 @@
 #include "pa2m.h"
+#include "src/lista.h"
 
-void prueba_simple()
+typedef struct nodo {
+	void *elemento;
+	struct nodo *siguiente;
+} Nodo;
+struct lista {
+	Nodo *primer_nodo;
+};
+
+
+void lista_crear_prueba()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i == j, "i es igual a j (%d == %d)", i, j);
-	pa2m_afirmar(2*i == 10, "2*i es igual a 2 (%d)", 2*i);
+	Lista *lista = lista_crear();
+	pa2m_afirmar(lista != NULL, "lista se creo correctamente");
+	lista_destruir(lista);
+}
+
+void lista_cantidad_elem_vacio()
+{
+	Lista *lista = lista_crear();
+	pa2m_afirmar(lista_cantidad_elementos(lista) == 0, "lista vacia devuelve 0");
+	lista_destruir(lista);
+}
+
+void lista_cantidad_elem()
+{
+	Lista *lista = lista_crear();
+	Nodo nodo;
+	Nodo nodo2;
+	nodo.siguiente = &nodo2;
+	nodo2.siguiente = NULL;
+	lista->primer_nodo = &nodo;
+	pa2m_afirmar(lista_cantidad_elementos(lista) == 2, "lista con 2 nodos devuelve 2");
+	lista_destruir(lista);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo("============== ??? ===============");
-	prueba_simple();
+	pa2m_nuevo_grupo("Lista");
+	lista_crear_prueba();
+	lista_cantidad_elem_vacio();
+	lista_cantidad_elem();
 
 	return pa2m_mostrar_reporte();
 }
--- src/lista.c
+++ src/lista.c
@@ -1,1 +1,37 @@
 #include "lista.h"
+
+typedef struct nodo {
+	void *elemento;
+	struct nodo *siguiente;
+} Nodo;
+
+struct lista {
+	Nodo *primer_nodo;
+};
+
+Lista *lista_crear()
+{
+	Lista *lista = malloc(sizeof(Lista));
+	lista->primer_nodo = NULL;
+	return lista;
+}
+
+size_t lista_cantidad_elementos(Lista *lista)
+{
+	if (!lista->primer_nodo || !lista)
+		return 0;
+	Nodo *actual = lista->primer_nodo;
+	size_t contador = 0;
+	while (actual != NULL) {
+		actual = actual->siguiente;
+		contador++;
+	}
+	return contador;
+}
+
+void lista_destruir(Lista *lista)
+{
+	if (!lista)
+		return;
+	free(lista);
+}
